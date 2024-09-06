5d94edd11efbbdd1ecc0ec070b295fd01c3a25d9
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-09-06 14:14:33 -0300

    frequencies TP

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -1,19 +1,67 @@
-module Frequencies  (Frequency, frequencyMap, frequencies, insert, insertionSort) where
+module Main where
 
 import Data.Map(Map)
 import qualified Data.Map as Map
 import Data.Tuple(swap)
+import System.Environment (getArgs)
+import Distribution.PackageDescription (mainLibSet)
 
 type Frequency = (Int, Char)
 
 frequencies::String -> [Frequency]
-frequencies = error "Implement it"
+frequencies xs = insertionSort (map swap (Map.toList (frequencyMap xs)))
 
 frequencyMap::(Ord a) => [a] -> Map a Int
-frequencyMap = error "Implement it"
+frequencyMap (x:xs) = Map.insertWith (+) x 1 (frequencyMap xs)
+frequencyMap [] = Map.empty
 
 insert::(Ord a) => a -> [a] -> [a]
-insert = error "Implement it"
+insert e [] = [e]
+insert e (x:xs)
+  | e <= x    = e : x : xs
+  | otherwise = x : insert e xs
 
-insertionSort :: (Ord a) => [a] -> [a]
-insertionSort = error "Implement it"
+reverseInsert :: (Ord a) => a -> [a] -> [a]
+reverseInsert e [] = [e]
+reverseInsert e (x:xs)
+  | e >= x    = e : x : xs
+  | otherwise = x : reverseInsert e xs
+
+insertionSort::(Ord a) => [a] -> [a]
+insertionSort (x:xs) = insert x (insertionSort xs)
+insertionSort [] = []
+
+{-
+4.Create a `main` function that:
+* Takes as an argument a file name (Use `getArgs`)
+* If the file name is not specified prints an error message
+* Reads the file (Using `readFile`).
+* Counts the frequencies of the letters in the file
+* Prints the table of frequencies in **REVERSE** order (Starting from the higher ones).
+
+```shell
+> freq example.txt | head
+' ': 292
+'e': 148
+'o': 124
+'t': 98
+'h': 96
+'n': 95
+'a': 93
+'s': 92
+'r': 68
+'i': 65
+-}
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> putStrLn "Error. Usage: freq <filename>"
+        (x:_) -> do
+            content <- readFile x
+            let freqs = reverse (frequencies content)
+            mapM_ print freqs
+            
+
+            
--- haskell-exercises/frequencies/src/Trie.hs
+++ haskell-exercises/frequencies/src/Trie.hs
@@ -1,23 +1,37 @@
 module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
-  
-data Trie a = Leaf a
-            | Trie a :-: Trie a deriving (Eq, Show, Ord)
 
--- Bring definition from previous TP
+data Trie a = Leaf a | Trie a :-: Trie a deriving (Eq, Show)
+trie :: Trie Char
+trie = (Leaf 'A' :-: Leaf 'B') :-: Leaf 'C'
 
-left::Trie a -> Trie a
-left = error "Define it"
+left :: Trie a -> Trie a
+left (l :-: _) = l
+left (Leaf _) = error "Left of: Leaf"
 
-right::Trie a -> Trie a
-left = error "Define it"
+right :: Trie a -> Trie a
+right (_ :-: r) = r
+right (Leaf _) = error "Right of: Leaf"
   
 find::Bits -> Trie a -> a
-find = error "Define it"
+find (F:bs) (l :-: _) = find bs l
+find (T:bs) (_ :-: r) = find bs r
+find [] (Leaf a) = a
+find _ _ = error "Invalid path"
 
-decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode :: Bits -> Trie Char -> String
+decode bits trie = decodeHelper bits trie
+    where
+        decodeHelper :: Bits -> Trie Char -> String
+        decodeHelper [] (Leaf c) = [c]
+        decodeHelper [] _ = []
+        decodeHelper (F:bs) (l :-: _) = decodeHelper bs l
+        decodeHelper (T:bs) (_ :-: r) = decodeHelper bs r
+        decodeHelper bits (Leaf c) = c : decodeHelper bits trie
 
-toList::Trie a -> [(a, Bits)]
-toList = error "Define it"            
+
+
+toList :: Trie a -> [(a, Bits)]
+toList (Leaf a) = [(a, [])]
+toList (l :-: r) = map (\(a, bs) -> (a, F:bs)) (toList l) ++ map (\(a, bs) -> (a, T:bs)) (toList r)
